9dad357c3837f8ae0c515ed36c8a1437363f8940
Edward L <59051661+Justedlev@users.noreply.github.com>
2023-01-10 20:08:33 +0200

    -

--- jaccount-server/src/main/java/com/justedlev/account/configuration/AppConfiguration.java
+++ jaccount-server/src/main/java/com/justedlev/account/configuration/AppConfiguration.java
@@ -1,34 +1,19 @@
 package com.justedlev.account.configuration;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.google.i18n.phonenumbers.PhoneNumberUtil;
 import org.apache.commons.validator.routines.EmailValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfiguration {
     @Bean
-    public ObjectMapper getObjectMapper() {
-        return new ObjectMapper()
-                .registerModule(new JavaTimeModule());
-    }
-
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public PhoneNumberUtil getPhoneNumberUtil() {
+    public PhoneNumberUtil phoneNumberUtil() {
         return PhoneNumberUtil.getInstance();
     }
 
     @Bean
-    public EmailValidator getEmailValidator() {
+    public EmailValidator emailValidator() {
         return EmailValidator.getInstance();
     }
 }
