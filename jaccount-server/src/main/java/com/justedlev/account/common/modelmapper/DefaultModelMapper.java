f14cb298a1b19ff648f6c4e2c19b472266b75776
Edward L <59051661+Justedlev@users.noreply.github.com>
2023-01-24 04:41:29 +0200

    -

---
+++ jaccount-server/src/main/java/com/justedlev/account/common/modelmapper/DefaultModelMapper.java
@@ -0,0 +1,7 @@
+package com.justedlev.account.common.modelmapper;
+
+import org.springframework.stereotype.Component;
+
+@Component("defaultMapper")
+public class DefaultModelMapper extends BaseModelMapper {
+}
